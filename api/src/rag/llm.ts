b83dfaee93b597fa8ead943bc705a6c6a2d68648
Amar-dev1 <3marofficial249@gmail.com>
2026-06-02 17:33:21 +0200

    added fallback mechanism

--- api/src/rag/llm.ts
+++ api/src/rag/llm.ts
@@ -1,9 +1,53 @@
 import { ChatGroq } from "@langchain/groq";
 
 export function getLLM(env: any) {
-  return new ChatGroq({
+  const primaryLLM = new ChatGroq({
     apiKey: env.GROQ_API_KEY,
-    model: "llama-3.3-70b-versatile",
+    model: "openai/gpt-oss-120b",
     temperature: 0.1,
+    maxRetries: 0,
+    callbacks: [
+      {
+        handleLLMError: (err: any) => {
+          console.error(
+            `Error on Primary LLM : gpt-oss-120b`,
+            JSON.stringify(err, null, 2),
+          );
+        },
+      },
+      {
+        handleLLMStart : ()=>{
+          console.log('Starting with the primary llm : openai/gpt-oss-120b');
+          
+        }
+
+      }
+    ],
   });
+
+  const fallbackLLM = new ChatGroq({
+    apiKey: env.GROQ_API_KEY,
+    model: "llama-3.1-8b-instant",
+    temperature: 0.1,
+    maxRetries: 0,
+    callbacks: [
+      {
+        handleLLMError: (err: any) => {
+          console.error(
+            `Error on Primary LLM : llama-3.1-8b-instant`,
+            JSON.stringify(err, null, 2),
+          );
+        },
+      },
+      {
+        handleLLMStart: ()=>{
+          console.log('starting fallback LLM with : llama-3.1-8b-instant');         
+        }
+      }
+    ],
+  });
+
+  return primaryLLM.withFallbacks({
+    fallbacks: [fallbackLLM]
+  })
 }
